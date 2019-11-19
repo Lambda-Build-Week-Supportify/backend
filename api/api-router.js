022f8f83b8d6bec18257638e7e092fdef8a9c277
Greg <gsc229@gmail.com>
2019-11-19 00:41:21 -0500

    disabling restricted middleware

--- api/api-router.js
+++ api/api-router.js
@@ -4,8 +4,6 @@ const usersRouter = require('../users/users-router');
 const schoolsRouter = require('../schools/schools-router');
 const usersSchoolsRouter = require('../users_schools/users-schools-router');
 const issuesRouter = require('../issues/issues-router');
-
-
 router.use('/auth', authRouter);
 router.use('/users', usersRouter);
 router.use('/schools', schoolsRouter);
@@ -16,5 +14,7 @@ router.use('/issues', issuesRouter);
 router.get('/', (req, res) => {
   res.json({ api: "THIS IS YOUR BASE API ROUTER. OTHERS: /api/auth  /api/users " })
 })
+
+
 
 module.exports = router;
--- users/users-router.js
+++ users/users-router.js
@@ -2,8 +2,8 @@ const router = require('express').Router();
 const Users = require('./users-model');
 const restricted = require('../auth/restricted-middleware');
 
-// !! don't forget to replace restricted middleware ^^
-router.get('/', restricted, (req, res) => {
+// !! don't forget to replace restricted middleware restricted, ^^
+router.get('/', (req, res) => {
 
   console.log("users-router.js decodedToken", req.decodedJwt)
   Users.find()
