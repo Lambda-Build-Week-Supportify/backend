62e67065cd38d025bc5598f219145a23aecca2b3
Greg <gsc229@gmail.com>
2019-11-20 02:19:46 -0500

    issue deletion restricted to nonboard members

--- issues/issues-router.js
+++ issues/issues-router.js
@@ -63,7 +63,7 @@ router.put("/:id", (req, res) => {
 
 router.delete("/:id", restricted, (req, res) => {
   const issues_id = req.params.id;
-  console.log(req.decodedJwt)
+  console.log("issues_id", issues_id)
   const board = req.decodedJwt.board;
   if (!board) {
     console.log('you have reached it')
