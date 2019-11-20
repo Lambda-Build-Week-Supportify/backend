76df711ab0d26ec353e54866d42f1eb9e4d744a5
Greg <gsc229@gmail.com>
2019-11-20 18:57:26 -0500

    get comments, get comment by issues_id, post a comment

--- api/api-router.js
+++ api/api-router.js
@@ -4,12 +4,16 @@ const usersRouter = require("../users/users-router");
 const schoolsRouter = require("../schools/schools-router");
 const usersSchoolsRouter = require("../users_schools/users-schools-router");
 const issuesRouter = require("../issues/issues-router");
+const commentsRouter = require("../x-comments/comments-router");
 
 router.use("/auth", authRouter);
 router.use("/users", usersRouter);
 router.use("/schools", schoolsRouter);
 router.use("/users-schools", usersSchoolsRouter);
 router.use("/issues", issuesRouter);
+router.use("/comments", commentsRouter);
+
+
 
 router.get("/", (req, res) => {
   res.json({
