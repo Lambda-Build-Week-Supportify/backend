491f89444c32a409fdf72aeab0e2395cb3fb2ac7
Greg <gsc229@gmail.com>
2019-11-20 23:23:50 -0500

    sent catch message in user delete

--- users/users-router.js
+++ users/users-router.js
@@ -42,7 +42,7 @@ router.delete("/:id", restricted, (req, res) => {
           res.sendStatus(204)
         } else { res.status(404).json({ message: "That user id does not exist" }) }
       })
-      .catch(err => res.status(500));
+      .catch(err => res.status(500).json({ message: "Internal server error." }))
 });
 
 // will add auth/validation middleware here
